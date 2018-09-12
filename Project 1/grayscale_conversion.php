22f84c3ccf814af499cd00ea8ecd41383bbecda9
Tyler <tyler.bruno@sjsu.edu>
2018-09-12 14:56:57 -0700

    Added basick function

---
+++ Project 1/grayscale_conversion.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tyler
+ * Date: 9/12/2018
+ * Time: 2:47 PM
+ */
+
+function averaging($red,$green,$blue){
+//Grey = ( r + g + b ) /3
+}
+
+function lightness($red,$green,$blue){
+//Grey = ( max(r, g, b) + min(r, g, b)) /2
+}
+
+function luminess($red,$green,$blue){
+//Grey = w1 * r + w2 * g + w3 * b; where w1 + w2 + w3 = 1; try w1 = .3 w2 = .6 w3 = .1).
+}
+
+function convertImageToGrayScale( $fileNameIn , $fileNameOut, $method){
+
+}
+
+
+
+?>
